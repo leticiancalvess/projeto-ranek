fded98e1cca756b219515d9645ec78b3a0f27684
Leticia <leticiancalves1@gmail.com>
2022-02-07 01:41:55 -0300

    lista de produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,12 +1,37 @@
 <template>
-  <section>
-    <h1>Lista</h1>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "home"
+  data() {
+    return {
+      produtos: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then(response => response.json())
+        .then(response => {
+          this.produtos = response;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
 };
 </script>
 
