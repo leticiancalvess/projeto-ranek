95349cfaa494c9569c1a56e98efa74b4b6230db8
Leticia <leticiancalves1@gmail.com>
2022-02-06 21:31:51 -0300

    home

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Lista</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
+</template>
+
+<script>
+import ProdutosBuscar from "../components/ProdutosBuscar.vue";
+import ProdutosLista from "../components/ProdutosLista.vue";
+
+export default {
+  name: "home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
