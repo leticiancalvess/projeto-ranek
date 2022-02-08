3ef893a8857a5b629d7a8f915949cdb8d7234e2c
Leticia <leticiancalves1@gmail.com>
2022-02-08 03:36:47 -0300

    filter number

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import router from "./router";
 import PaginaCarregando from "./components/PaginaCarregando.vue";
 
 Vue.component("PaginaCarregando", PaginaCarregando);
+
+Vue.filter("numeroPreco", valor => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  } else {
+    return "";
+  }
+});
+
 new Vue({
   router,
   render: h => h(App)
