4c548a89538ae1240ab8d1c2f22af96ecf3486c4
Leticia <leticiancalves1@gmail.com>
2022-02-10 17:45:10 -0300

    cep

--- src/services.js
+++ src/services.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000" //url base 
-})//cria uma nova instancia de axios
+  baseURL: "http://localhost:3000" //url base
+}); //cria uma nova instancia de axios
 
 export const api = {
   get(endpoint) {
     return axiosInstance.get(endpoint);
   }
 };
+
+export function getCep(cep) {
+  return axios.get(`https://viacep.com.br/ws/${cep}/json`);
+}
